d654f7afb86a44101d3864eb3215af3766819883
Daniel <triotk@gmail.com>
2021-04-12 21:16:18 +0900

    fb fix

--- apps/client/demo-7/lib/firebase-init.ts
+++ apps/client/demo-7/lib/firebase-init.ts
@@ -3,13 +3,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const config = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  apiKey: 'AIzaSyDIwL1_7rXo8GH30Ue_VNmsa1nGF2THyv8',
+  authDomain: 'hessed.firebaseapp.com',
+  databaseURL: 'https://hessed-default-rtdb.firebaseio.com',
+  projectId: 'hessed',
+  storageBucket: 'hessed.appspot.com',
+  messagingSenderId: '427061924743',
+  appId: '1:427061924743:web:efcb71917b03af383fb922',
+  measurementId: 'G-BFPYCVFF0C',
 };
 
 if (!firebase.apps.length) {
