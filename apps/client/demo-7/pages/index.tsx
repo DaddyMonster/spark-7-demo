2756e66a971d554e4887f1334da41becebd76775
Daniel <triotk@gmail.com>
2021-04-13 08:19:16 +0900

    firebase-fix-test

--- apps/client/demo-7/pages/index.tsx
+++ apps/client/demo-7/pages/index.tsx
@@ -35,7 +35,7 @@ export const Index: CustomPageType = () => {
                     lineHeight: '3rem',
                   }}
                 >
-                  전 세계의 사람들과 7분간의 편안한 대화 <br />
+                  전 세계의 사람들과 7분간의 편안한 대화<br />
                   지금 시작하세요
                 </Typography>
 
