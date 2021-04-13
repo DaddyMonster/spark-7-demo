f0253d697622db9d901fa73e36fd9f3f9338a862
Daniel <triotk@gmail.com>
2021-04-14 06:43:55 +0900

    pwa-fix

--- apps/client/demo-7/next.config.js
+++ apps/client/demo-7/next.config.js
@@ -1,22 +1,25 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
+const withPlugins = require('next-compose-plugins');
 const withPWA = require('next-pwa');
-module.exports = withNx({
-  webpack(config) {
-    // Prevent nx from adding an svg handler - stick to what is provided by
-    // nextjs or that we have defined ourselves.
-    config.module.rules.push = (...items) => {
-      Array.prototype.push.call(
-        config.module.rules,
-        ...items.filter((item) => item.test.toString() !== '/\\.svg$/')
-      );
-    };
 
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+console.log(withPWA);
 
-    return withPWA({ ...config, dest: 'public' });
-  },
-});
+module.exports = withPlugins([
+  [withPWA, { pwa: { dest: 'public' } }],
+  [
+    withNx,
+    {
+      webpack(config) {
+        config.module.rules.push = (...items) => {
+          Array.prototype.push.call(
+            config.module.rules,
+            ...items.filter((item) => item.test.toString() !== '/\\.svg$/')
+          );
+        };
+
+        return config;
+      },
+    },
+  ],
+]);
